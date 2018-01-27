ff19747285c646c4e66f15244b6df669280fef3e
rabenda <rabenda.cn@gmail.com>
2018-01-27 20:04:58 +0800

    StoneLang finish Lexer and AST

---
+++ src/stone/ast/ASTLeaf.java
@@ -0,0 +1,44 @@
+package stone.ast;
+
+import stone.Token;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ASTLeaf extends ASTree {
+    private static ArrayList<ASTree> empty = new ArrayList<>();
+    protected Token token;
+
+    public ASTLeaf(Token t) {
+        token = t;
+    }
+
+    @Override
+    public ASTree child(int i) {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public int numChildren() {
+        return 0;
+    }
+
+    @Override
+    public Iterator<ASTree> children() {
+        return empty.iterator();
+    }
+
+    @Override
+    public String toString() {
+        return token.getText();
+    }
+
+    @Override
+    public String location() {
+        return "at line " + token.getLineNumber();
+    }
+
+    public Token token() {
+        return token;
+    }
+}
